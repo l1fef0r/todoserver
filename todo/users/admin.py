cc68fede05925e606c2b5c631f2ce9f5b0511aa4
l1fef0r <alexunder.stepanoff@gmail.cm>
2021-07-22 00:53:46 +0500

    Добавил модели TODO и Project, зарегистрировал их в админки, для обновления или удаления. Связал все записи ключами

--- todo/users/admin.py
+++ todo/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Todo, Project
 from django.contrib import admin
 
 admin.site.register(UserProfile)
+admin.site.register(Todo)
+admin.site.register(Project)
 
--- todo/users/views.py
+++ todo/users/views.py
@@ -1,9 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 
-from .models import UserProfile
-from .serializers import UserSerializer
+from .models import UserProfile, Todo, Project
+from .serializers import UserSerializer, TodoSerializer, ProjectSerializer
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = UserProfile.objects.all()
+
+class ProjectViewSet(ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
+
+class TodoViewSet(ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
 
